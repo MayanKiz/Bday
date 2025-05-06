2f823203bcb6f68a2d62df605281632db746a510
MayanKiz <140157357+MayanKiz@users.noreply.github.com>
2025-05-07 02:16:26 +0530

    Update countdown.jsx

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -21,7 +21,7 @@ function calculateTimeLeft(targetDate) {
 }
 
 export default function Countdown() {
-  const targetDate = new Date("2025-06-07T00:00:00")
+  const targetDate = new Date("2025-05-07T00:00:00")
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(targetDate))
 
   useEffect(() => {
